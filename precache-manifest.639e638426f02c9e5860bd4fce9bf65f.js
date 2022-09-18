5a90fe82fbeec31b3f983b50ce1a63cf3fc283d5
Dagim01 <Daginaruto@gmail.com>
2022-09-18 09:29:14 +0300

    Updates

---
+++ precache-manifest.639e638426f02c9e5860bd4fce9bf65f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9c872ae2b6d46e49a70",
+    "url": "/Dagim-Yesuf/static/css/main.3c4adce8.chunk.css"
+  },
+  {
+    "revision": "d9c872ae2b6d46e49a70",
+    "url": "/Dagim-Yesuf/static/js/main.d9c872ae.chunk.js"
+  },
+  {
+    "revision": "5d620098248379f2243a",
+    "url": "/Dagim-Yesuf/static/js/1.5d620098.chunk.js"
+  },
+  {
+    "revision": "e9dd4836e764b7f22f3c",
+    "url": "/Dagim-Yesuf/static/js/runtime~main.e9dd4836.js"
+  },
+  {
+    "revision": "170a613caaa8d1002d170a7b81e17568",
+    "url": "/Dagim-Yesuf/static/media/cv.170a613c.pdf"
+  },
+  {
+    "revision": "a6b75f7d392036599e0b9fa1159e02a8",
+    "url": "/Dagim-Yesuf/static/media/Dagim4.a6b75f7d.png"
+  },
+  {
+    "revision": "031957a2d6d151c1cfa9743b8046a874",
+    "url": "/Dagim-Yesuf/static/media/Dagim4.031957a2.jpg"
+  },
+  {
+    "revision": "758ac51356d9c7902c9273e591a4702e",
+    "url": "/Dagim-Yesuf/static/media/rateit.758ac513.png"
+  },
+  {
+    "revision": "760e76569b0c7300e90445fd273438e8",
+    "url": "/Dagim-Yesuf/static/media/Internsuit.760e7656.png"
+  },
+  {
+    "revision": "856129df762d7bf62a17687c90f80078",
+    "url": "/Dagim-Yesuf/static/media/esa1.856129df.jpg"
+  },
+  {
+    "revision": "170b2bba7048acbb7ef07424405abe1a",
+    "url": "/Dagim-Yesuf/static/media/background.170b2bba.jpg"
+  },
+  {
+    "revision": "c9fb262a5f999c96a25d53cccf3b4cc1",
+    "url": "/Dagim-Yesuf/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Dagim-Yesuf/precache-manifest.639e638426f02c9e5860bd4fce9bf65f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Dagim-Yesuf/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
