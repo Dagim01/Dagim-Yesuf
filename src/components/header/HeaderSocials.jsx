97a6ac799002290408ebe616336e6a29dae0a593
Dagmawi Yesuf Mohammed <daginaruto@gmail.com>
2023-09-06 17:32:07 +0300

    Updated Socials

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
 import {BsDribbble} from 'react-icons/bs'
+import {BsBehance} from 'react-icons/bs'
 
 const classname  = () => {
   return (
     <div class ="header__socials">
         <a href="https://www.linkedin.com/in/dagmawiyesuf/" target="_blank"><BsLinkedin/></a>
         <a href='https://github.com/Dagim01' target="_blank"><BsGithub/></a>
-        <a href='https://t.me/rotund_form745' target="_blank"><BsDribbble/></a>
+        <a href='https://dribbble.com/Rotund_form745' target="_blank"><BsDribbble/></a>
+        <a href='https://www.behance.net/dagimyesuf' target="_blank"><BsBehance/></a>
     </div>
   )
 }
