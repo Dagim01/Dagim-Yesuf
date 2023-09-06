ea014a267bca88d8001970cb4d2ee88ed9ec1aef
Dagmawi Yesuf Mohammed <daginaruto@gmail.com>
2023-09-06 17:28:44 +0300

    Updated Dribbble

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsTelegram} from 'react-icons/bs'
+import {BsDribbble} from 'react-icons/bs'
 
 const classname  = () => {
   return (
