9acf306cac43e73ec4a1dde908e94635d0377963
Dagmawi Yesuf Mohammed <daginaruto@gmail.com>
2023-09-06 17:23:05 +0300

    dd

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="container header__container">
         <h5> Hello I'm</h5>
         <h1> Dagmawi Yesuf</h1>
-        <h5 className="text-light"> HoE | UI-UX Designer</h5>
+        <h5 className="text-light"> Software Engineer | UI-UX Designer</h5>
         <CTA/>
         <HeaderSocials />
 
