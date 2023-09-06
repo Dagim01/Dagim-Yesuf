3fb2e8197128bd721cfe4c7e5e483c6b615f736a
Dagmawi Yesuf Mohammed <daginaruto@gmail.com>
2023-09-06 11:49:05 +0300

    Updated About page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,12 +23,12 @@ const About = () => {
             <article className='about__card'>
               <FaAward class= 'about__icon'/>
               <h5>Work Experience</h5>
-              <small>9 months</small>
+              <small>3 years</small>
             </article>
             <article className='about__card'>
               <FiUsers class= 'about__icon'/>
               <h5>Affiliations</h5>
-              <small>Social Media Manager at A2SV | Africa to Silicon Valley</small>
+              <small>UI-UX Designer and Social Media Manager at A2SV | Africa to Silicon Valley</small>
             </article>
             <article className='about__card'>
               <BsFolderCheck class= 'about__icon'/>
@@ -36,9 +36,7 @@ const About = () => {
               <small>5 Completed Projects</small>
             </article>
           </div>
-          <p>I'm a student at Addis Ababa University pursuing a degree in Software Engineering.
-            I am very passionate about problem solving, programming, User Interface Design, User Experience and technology in general.
-            I really like playing Video Games and sending people to the Gulag ;)
+          <p>Hi, I'm Dagmawi Yesuf, a versatile software engineer and front-end developer with a passion for UX design, video production, graphics, and digital marketing. My career journey has led me to explore diverse facets of the digital world, culminating in a unique skill set that combines technical expertise with creative flair. 
           </p>
         <a href='#contact' className='btn btn-primary'>Let's talk</a>
         </div>
